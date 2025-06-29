c4293bc0c60d835851d3b894d01e2e0332cfd394
Youssef Ashraf <yusuufashraaf@icloud.com>
2025-06-29 04:24:19 +0300

    chore(setup): - configure standalone Angular app with routing and bootstrap - apply Noto Serif font and bootstrap base styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { BrowseBooks } from './components/browse-books/browse-books';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'Home', component: Home },
+  { path: 'Browse', component: BrowseBooks },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './shared/components/navbar/navbar';
+import { Search } from './shared/components/search/search';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, Search, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'NovelNest';
