e1df1593e72dc7ae692fdc4967f0fe8f487ad5e0
Ahmed Kamel <ahmedkamel180998@gmail.com>
2025-06-29 20:55:52 +0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { BrowseBooks } from './components/browse-books/browse-books';
+import { Cart } from './components/cart/cart';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'Home', pathMatch: 'full' },
   { path: 'Home', component: Home },
   { path: 'Browse', component: BrowseBooks },
+  { path: 'Cart', component: Cart },
 ];
