7ffabbb2633a6c78988a1050dfa1a83ac80cd0ca
mahmoud <mahmoudabdelhaleem90@gmail.com>
2025-06-30 00:54:03 +0300

    connecting with endPoints of Payments part-1

---
+++ src/app/components/payment-success/payment-success.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-success',
+  imports: [],
+  templateUrl: './payment-success.html',
+  styleUrl: './payment-success.css'
+})
+export class PaymentSuccess {
+
+}
