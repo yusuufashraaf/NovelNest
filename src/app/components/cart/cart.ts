e606da1ce522eef34324a3cb7bfa4bea4d7a01ef
Ahmed Kamel <ahmedkamel180998@gmail.com>
2025-06-30 13:35:29 +0300

    feat(cart): implement shopping cart layout and functionality

--- src/app/components/cart/cart.ts
+++ src/app/components/cart/cart.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { CartItem } from './cart-item/cart-item';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [CartItem],
   templateUrl: './cart.html',
-  styleUrl: './cart.css'
+  styleUrl: './cart.css',
 })
 export class Cart {
-
+  cartService = inject(CartService);
+  totalPrice = this.cartService
+    .cart()
+    .reduce((total, item) => total + item.price, 0)
+    .toFixed(2);
 }
