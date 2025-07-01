304ce684f2050aeffd346eab63a10cc36751d572
Youssef Ashraf <yusuufashraaf@icloud.com>
2025-07-01 14:51:07 +0300

    feat:Add user tabs for profile

---
+++ src/app/components/profile-tabs/profile-tabs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-tabs',
+  imports: [],
+  templateUrl: './profile-tabs.html',
+  styleUrl: './profile-tabs.css'
+})
+export class ProfileTabs {
+
+}
