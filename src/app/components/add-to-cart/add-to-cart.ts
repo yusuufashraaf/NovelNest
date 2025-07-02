e1df2322f71937c9abbd687cde518677dcf6c7b4
Ahmed Kamel <ahmedkamel180998@gmail.com>
2025-07-02 11:51:57 +0300

    feat(cart): integrate sweetalert2 for success and error notifications in add-to-cart and remove-item functionalities

--- src/app/components/add-to-cart/add-to-cart.ts
+++ src/app/components/add-to-cart/add-to-cart.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CartService } from '../../services/cart.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-to-cart',
@@ -15,12 +16,25 @@ export class AddToCart {
   addToCart() {
     if (!this.productId) return;
     this.cartService.addToCart(this.productId, 1).subscribe({
-      next: () => {
-        // Optionally show a success message or update cart icon
-        alert('Added to cart!');
+      // update cart icon simultaneously
+      next: (res) => {
+        const updatedCart = this.cartService.cart();
+        this.cartService.cart.set({
+          ...updatedCart,
+          totalQuantity: res.data.totalQuantity,
+        });
+        // show success message with sweetalert2
+        Swal.fire({
+          title: 'Added to cart!',
+          icon: 'success',
+        });
       },
       error: () => {
-        alert('Failed to add to cart.');
+        Swal.fire({
+          // show error message with sweetalert2
+          title: 'Failed to add to cart.',
+          icon: 'error',
+        });
       },
     });
   }
