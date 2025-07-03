38d00245d94ca53318e94f62f9f7a84a268d3df4
Mohamed Mostafa <thepcvirus@gmail.com>
2025-07-03 20:51:27 +0200

    Dashboard Frontend Finished(Almost)

--- src/app/components/dashboard-products/dashboard-products.ts
+++ src/app/components/dashboard-products/dashboard-products.ts
@@ -2,6 +2,7 @@ import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { Product } from '../../services/product.service';
 import { ActivatedRoute } from '@angular/router';
 import { Products } from '../models/product.model';
+import e from 'express';
 
 @Component({
   selector: 'app-dashboard-products',
@@ -13,6 +14,7 @@ export class DashboardProducts {
   booksInventory: Products[] = [//list of all books in the store
 
   ];
+  state:number = 0;
 
   constructor(
     private productService: Product,
@@ -54,10 +56,29 @@ export class DashboardProducts {
 
   editBook(bookId: string): void {
     console.log('Edit book:', bookId);
+    if(this.state == 2)
+      this.state = 0;
+    else
+    this.state = 2;
   }
 
   deleteBook(bookId: string): void {
     console.log('Delete book:', bookId);
     this.booksInventory = this.booksInventory.filter(book => book._id !== bookId);
+  }
+
+  canAddBook(){
+    if(this.state == 1)
+      this.state = 0;
+    else
+    this.state = 1;
+  }
+
+  addBook(){
+    //add book logic
+  }
+
+  updateBook(){
+    //update book logic
   }
 }
