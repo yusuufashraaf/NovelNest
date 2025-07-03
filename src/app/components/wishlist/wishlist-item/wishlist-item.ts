3d604881b2a7e02fba0402b47981ec27c922eed2
Ahmed Kamel <ahmedkamel180998@gmail.com>
2025-07-03 14:27:56 +0300

    feat(wishlist): add WishlistItem component with HTML, CSS, and TypeScript for displaying and managing wishlist items

---
+++ src/app/components/wishlist/wishlist-item/wishlist-item.ts
@@ -0,0 +1,66 @@
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { AddToCart } from '../../add-to-cart/add-to-cart';
+import { WishlistService } from '../../../services/wishlist.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-wishlist-item',
+  standalone: true,
+  imports: [AddToCart],
+  templateUrl: './wishlist-item.html',
+  styleUrl: './wishlist-item.css',
+})
+export class WishlistItem {
+  @Input() item!: {
+    productId: string;
+    title: string;
+    author: string;
+    img: string;
+  };
+  wishlistService = inject(WishlistService);
+
+  @Output() remove = new EventEmitter<string>();
+
+  removeItem() {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'This item will be removed from your wishlist.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: 'Deleted!',
+          text: 'Your item has been removed from your wishlist.',
+          icon: 'success',
+        });
+        this.remove.emit(this.item.productId);
+      }
+    });
+  }
+
+  wishlistHandler(productId: string) {
+    if (!productId) return;
+    this.wishlistService.deleteFromWishlist(productId).subscribe({
+      next: (res) => {
+        const previousWishlist = this.wishlistService.wishlist();
+        this.wishlistService.wishlist.set({
+          ...previousWishlist,
+          wishlistItems: previousWishlist.wishlistItems.filter(
+            (item: any) => item.productId !== productId
+          ),
+          totalQuantity: res.data.totalQuantity,
+        });
+      },
+      error: () => {
+        Swal.fire({
+          title: 'Failed to remove from wishlist.',
+          icon: 'error',
+        });
+      },
+    });
+  }
+}
