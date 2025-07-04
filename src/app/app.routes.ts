062807f3438e0d4bf06d9fe06ff04ce4f90504cf
Youssef Ashraf <yusuufashraaf@icloud.com>
2025-07-04 09:49:27 +0300

    refactor: Change routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,37 +9,28 @@ import { PaymentError } from './components/payment-error/payment-error';
 import { Dashboard } from './components/dashboard/dashboard';
 import { ThankYou } from './components/thank-you/thank-you';
 import { UserProfile } from './components/user-profile/user-profile';
-<<<<<<< Updated upstream
-=======
 import { Wishlist } from './components/wishlist/wishlist';
->>>>>>> Stashed changes
 import { PurchasedBooks } from './components/user-profile/profile-tabs/purchased-books/purchased-books';
 import { PersonalInfo } from './components/user-profile/profile-tabs/personal-info/personal-info';
-import { Wishlist } from './components/wishlist/wishlist';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'Home', pathMatch: 'full' },
   { path: 'Home', component: Home },
   { path: 'Browse', component: BrowseBooks },
-  { path: 'Browse/:id', component:BookDetails },
   { path: 'Cart', component: Cart },
-  { path: 'Wishlist', component: Wishlist },
   { path: 'ContactUs', component: ContactUs },
   { path: 'checkout', component: CheckOut },
   { path: 'success', component: PaymentSuccess },
   { path: 'err', component: PaymentError },
-  { path: 'dashboard', component: Dashboard },
+  {path:'dashboard',component:Dashboard},
   { path: 'thank-you', component: ThankYou },
   {
     path: 'profile',
     component: UserProfile,
     children: [
       { path: 'purchased', component: PurchasedBooks },
-      { path: 'info', component: PersonalInfo },
-      { path: '', redirectTo: 'purchased', pathMatch: 'full' },
       {path:'info',component:PersonalInfo},
-      {path:'reviews',component:PersonalReviews},
-      { path: '', redirectTo: 'purchased', pathMatch: 'full' },
+      { path: '', redirectTo: 'purchased', pathMatch: 'full' }, 
     ],
   },
 ];
