833980582da27faa06d745d313782b19a92205f7
Youssef Ashraf <yusuufashraaf@icloud.com>
2025-08-12 18:01:23 +0300

    fix(api): fix api url

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl:
-    'https://1d8f222f-e6cd-4b30-8295-eee3fc85c4bc-00-x25a0w5n7axr.janeway.replit.dev',
+  apiUrl: 'https://novellist-backend-production.up.railway.app',
 };
+
+//    'https://1d8f222f-e6cd-4b30-8295-eee3fc85c4bc-00-x25a0w5n7axr.janeway.replit.dev',
